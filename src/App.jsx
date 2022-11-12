fd56b86f9e1c3daa1d5de48b1fdb76a1ecce3621
Rafael Franco <rafaelfr@outlook.com>
2022-11-12 09:55:13 -0300

    Add line options

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,20 @@
-import { useState, useEffect } from 'react'
+import parseNumericRange from 'parse-numeric-range'
 import ReactMarkdown from 'react-markdown'
-import { getHighlighter, setCDN } from 'shiki'
+import useShiki, { setCDN } from 'use-shiki'
 
 setCDN('https://cdn.jsdelivr.net/npm/shiki/')
 
 function App() {
-  const [highlighter, setHighlighter] = useState()
-
-  useEffect(() => {
-      getHighlighter({
-        theme: 'dracula',
-        langs: ['jsx'],
-      }).then(setHighlighter)
-  }, [])
+  const highlighter = useShiki({
+    theme: 'dracula',
+    langs: ['jsx'],
+  })
 
   const markdown = `
   # Here is some Markdown 
 
-  \`\`\`jsx 
+  \`\`\`jsx highlight=1&add=4&delete=3,5
   const [count, setCount] = useState(0)
-  \`\`\`  
-
-  \`\`\`jsx 
-  const [count, setCount] = useState(0) 
 
   useEffect(() => {
     setCount(count + 1)
@@ -30,20 +22,44 @@ function App() {
   \`\`\`
   `
 
-  return <ReactMarkdown
+  return (
+    <ReactMarkdown
       components={{
-        pre({ children }) {
+        pre({ children, node }) {
           const {
             className,
             children: [code],
           } = children[0].props
           const match = /language-(\w+)/.exec(className || '')
+
+          let lineOptions = []
+          if (typeof node.children[0].data?.meta === 'string') {
+            const options = Object.fromEntries([
+              ...new URLSearchParams(node.children[0].data?.meta).entries(),
+            ])
+
+            lineOptions = Object.entries(options).reduce(
+              (acc, [className, lines]) => {
+                const parsedLines = parseNumericRange(lines)
+                return [
+                  ...acc,
+                  ...parsedLines.map(line => ({
+                    classes: [className],
+                    line,
+                  })),
+                ]
+              },
+              [],
+            )
+          }
+
           return (
             <code
               dangerouslySetInnerHTML={{
                 __html:
-                  highlighter?.codeToHtml(code, {
+                  highlighter?.codeToHtml(code.replace(/\n$/, ''), {
                     lang: match?.[1],
+                    lineOptions,
                   }) ?? '',
               }}
             />
@@ -53,6 +69,7 @@ function App() {
     >
       {markdown}
     </ReactMarkdown>
+  )
 }
 
 export default App
